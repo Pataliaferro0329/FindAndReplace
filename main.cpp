34289ee67f4361b45b0c355cc01e6bb5d0b31110
Patrick Taliaferro <pataliaferro0329@gmail.com>
2020-05-08 10:37:54 -0400

    Need to debug the re-write section

--- main.cpp
+++ main.cpp
@@ -13,23 +13,57 @@ int main(int argc, char * argv[]) {
 
 	unsigned long ulOccuranceCount = 0; 
 	unsigned short usLengthOfReplacedString = 0;
+	unsigned short usLengthOfReplacingString = 0;
 	unsigned long ulSizeOfFile = 0;
 
 	while(argv[2][usLengthOfReplacedString] != '\0') usLengthOfReplacedString++;
+	while(argv[3][usLengthOfReplacingString] != '\0') usLengthOfReplacingString++;
 
 	std::ifstream ifs(argv[1],std::ifstream::in);
 	
-	while(ifs.get()) ulSizeOfFile++;
+	ifs.seekg(0,ifs.end);
+	ulSizeOfFile = ifs.tellg();
+	ifs.seekg(0);
 
 	char * cBuffer = new char[ulSizeOfFile];
 
-	//Read out file into cBuffer, Finding first substring and replacing it
-	//delete old file
-	//create new file and copy in cBuffer
-	//print out how many occurances there were
-
+	ifs.read(cBuffer,ulSizeOfFile);
 
 	ifs.close();
+
+	if (remove(argv[1]) == 0) std::cout << "Deleted Successfully\n";
+	else 
+	{
+		std::cout <<"remove() unsuccessful\n"; //rewrite 
+		return 0; 
+	}
+
+	
+	//create new file and copy in cBuffe
+	std::ofstream ofs(argv[1],std::ofstream::out);
+	int j;
+
+	for(int i = 0; i < ulSizeOfFile - usLengthOfReplacedString; i++)
+	{
+		if(cBuffer[i] == argv[2][1])
+		{
+			j = 1;
+			while(j <= usLengthOfReplacedString && cBuffer[i+j] == argv[2][j+1]) j++;		
+		}
+		if(j > usLengthOfReplacedString)
+		{
+			ofs.write(argv[3],usLengthOfReplacingString);
+			i += usLengthOfReplacedString;
+			i--;
+			ulOccuranceCount++;
+		}
+		else ofs.put(cBuffer[i]);
+	}
+
+	//print out how many occurances there were
+	std::cout << "Number of Changes: " << unsigned(ulOccuranceCount) << "\n";
+	ofs.close();
+
 	delete cBuffer;
 	return 0;
 }
