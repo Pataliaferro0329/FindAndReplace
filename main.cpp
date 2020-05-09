838201df118484d3100a6c2288fc0f232a96a880
Patrick Taliaferro <pataliaferro0329@gmail.com>
2020-05-09 10:12:12 -0400

    Finished debugging with a long example; Debugged for the special case that only a character is replaced

--- main.cpp
+++ main.cpp
@@ -16,59 +16,81 @@ int main(int argc, char * argv[]) {
 	unsigned short usLengthOfReplacingString = 0;
 	unsigned long ulSizeOfFile = 0;
 
+	//Find the size of both substrings
 	while(argv[2][usLengthOfReplacedString] != '\0') usLengthOfReplacedString++;
 	while(argv[3][usLengthOfReplacingString] != '\0') usLengthOfReplacingString++;
 
+	//create an input stream
 	std::ifstream ifs(argv[1],std::ifstream::in);
 	
+	//find the size of the file
 	ifs.seekg(0,ifs.end);
 	ulSizeOfFile = ifs.tellg();
+	//return to the beginning of the file
 	ifs.seekg(0);
 
+	//Place the file contents within a buffer
 	char * cBuffer = new char[ulSizeOfFile];
-
 	ifs.read(cBuffer,ulSizeOfFile);
 
+	//close the input stream
 	ifs.close();
 
+	//delete the file
 	if (remove(argv[1]) != 0)
 	{
 		std::cout <<"ERROR: File work error - remove(<File_Name>) != 0\n";
-	       	std::cout <<"Most Likely due to not high enough priveleges\n"; 
+	       	std::cout <<"Most Likely due to not executing with high enough privelege state\n"; 
 		return 0; 
-	}
-
-	//for(int i = 0; i < ulSizeOfFile; i++) std::cout << cBuffer[i];	
+	}	
 	
+	//create an output stream
 	std::ofstream ofs(argv[1],std::ofstream::out);
+
+	//This variable helps to find the substring later on
 	int j;
 
 	std::cout << "\nStart Parsing\n";
 
-	for(int i = 0; i < ulSizeOfFile - usLengthOfReplacedString; i++)
+	//In the special case that we are replacing a single character
+	if(usLengthOfReplacedString == 1)
 	{
-		j = 1;
-		if(cBuffer[i] == argv[2][0])
+		for(int i = 0; i < ulSizeOfFile; i++)
 		{
-			while(j < usLengthOfReplacedString && cBuffer[i+j] == argv[2][j]) j++;		
-		}
-		//std::cout << cBuffer[i] << " : " << j << "\n";
-		if(j == usLengthOfReplacedString)
-		{
-			ofs.write(argv[3],usLengthOfReplacingString);
-			i += usLengthOfReplacedString - 1;
-			ulOccuranceCount++;
-			//for(int k = 0; k < usLengthOfReplacedString; k++) std::cout << argv[3][k];
-		}
-		else 
-		{
-			ofs.put(cBuffer[i]);
-			//std::cout << cBuffer[i];
+			if(cBuffer[i] == argv[2][0]) 
+			{
+				ulOccuranceCount++;
+				ofs.write(argv[3],usLengthOfReplacingString);
+			}
+			else ofs.put(cBuffer[i]);
 		}
 	}
-	//Need to write the end of the buffer
-	ofs.write(&cBuffer[ulSizeOfFile - usLengthOfReplacedString],usLengthOfReplacedString);
-	//for(int i = usLengthOfReplacedString; i > 0; i--) std::cout << cBuffer[ulSizeOfFile - i];
+	else
+	{
+		//Check all possible spots within the buffer for the substring
+		for(int i = 0; i < ulSizeOfFile - usLengthOfReplacedString; i++)
+		{
+			j = 1;
+			if(cBuffer[i] == argv[2][0])
+			{
+				while(j < usLengthOfReplacedString && cBuffer[i+j] == argv[2][j]) j++;		
+			}
+			if(j == usLengthOfReplacedString)
+			{
+				ofs.write(argv[3],usLengthOfReplacingString);
+				i += usLengthOfReplacedString - 1;
+				ulOccuranceCount++;
+			}
+			else 
+			{
+				ofs.put(cBuffer[i]);
+				
+			}
+		}
+		
+		ofs.write(&cBuffer[ulSizeOfFile - usLengthOfReplacedString],usLengthOfReplacedString);
+		
+	}
 
 	//print out how many occurances there were
 	std::cout << "\nNumber of Changes: " << unsigned(ulOccuranceCount) << "\n";
